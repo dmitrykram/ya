8e3cf3b5d6115ba6426d6c8863b18a928c722551
dmitrykram <93586906+dmitrykram@users.noreply.github.com>
2022-05-20 00:40:31 +0400

    second commit

--- test.py
+++ test.py
@@ -1,24 +1,32 @@
-s = [int(i) for i in input().split()]
-t = []
-l = len(s)-1
-i = 0
-j = 0
-if len(s) == 1:
-    print()
-else: 
-    s.sort()
-    for st in s:
-        if s[i] in t:
-            i += 1
-            continue
-        if i == l:
-            break
-        elif s[i] == s[i+1]:
-            t.append(s[i])
-            i += 1
-        elif s[i] != s[i+1]:
-            i += 1
-            continue
-    for st2 in t:
-        print(str(t[j])+' ',end='')
-        j +=1
+def test():
+    test = input()
+    s = [int(i) for i in test.split()]
+    t = []
+    l = len(s)-1
+    i = 0
+    j = 0
+    if len(s) == 1:
+        print()
+    else: 
+        s.sort()
+        for st in s:
+            if s[i] in t:
+                i += 1
+                continue
+            if i == l:
+                break
+            elif s[i] == s[i+1]:
+                t.append(s[i])
+                i += 1
+            elif s[i] != s[i+1]:
+                i += 1
+                continue
+        for st2 in t:
+            print(str(t[j])+' ',end='')
+            j +=1
+    breakpoint()
+    print(1)
+    return t
+
+if __name__ == '__main__':
+    print(test())
